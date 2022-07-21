359117f5f9757037334be42fbe09ee6b0da03b14
Magdalena Wieczorek <magd.wieczorek@gmail.com>
2022-07-20 23:02:21 -0400

    6.21 Added kodilla-testing calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - calc");
+        Calculator calc = new Calculator();
+        if(calc.add(2,2)==4){
+            System.out.println("test OK");
+        } else {
+        System.out.println("Error!");
+         }
+        if(calc.subtract(4,2)==2){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
