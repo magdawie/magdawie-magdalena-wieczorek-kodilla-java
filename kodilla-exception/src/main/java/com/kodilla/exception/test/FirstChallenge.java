9d0b579457cb330bcc1f8da3086b44298d21f361
Magdalena Wieczorek <magd.wieczorek@gmail.com>
2022-08-18 15:19:26 -0400

    8.1 try

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,5 +1,10 @@
 package com.kodilla.exception.test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
@@ -12,8 +17,15 @@ public class FirstChallenge {
 
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
 
-        System.out.println(result);
+
+        //System.out.println(result);
+        try  {
+            double result = firstChallenge.divide(3, 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Oh no! Something went wrong! Error:" + e);
+        } finally {
+            System.out.println("I am gonna be here... always!");
+        }
     }
 }
