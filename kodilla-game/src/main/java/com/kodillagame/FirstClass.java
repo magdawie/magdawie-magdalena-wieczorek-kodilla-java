23a15aa1538bc1ff8f880148d60306df172bde12
Magdalena Wieczorek <magd.wieczorek@gmail.com>
2022-08-22 21:36:20 -0400

    1

---
+++ kodilla-game/src/main/java/com/kodillagame/FirstClass.java
@@ -0,0 +1,4 @@
+package com.kodillagame;
+
+public class FirstClass {
+}
---
+++ kodilla-game/src/test/java/com/kodillagame/KodillaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodillagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
